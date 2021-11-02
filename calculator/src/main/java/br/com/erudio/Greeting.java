3fd5b894f404eac13dd8b81cf679d8d50664cb39
@m.e.santos <maria.elaine@gubee.com.br>
2021-11-02 18:09:15 -0300

    initial commit

---
+++ calculator/src/main/java/br/com/erudio/Greeting.java
@@ -0,0 +1,17 @@
+package br.com.erudio;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Greeting {
+	
+	private final long id;
+	private final String content;
+	
+	
+
+}
